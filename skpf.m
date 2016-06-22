0b2f90a2d93bfdad600d14a35c7d90bb205aaf20
MOHAMED M S NASSER <mms.nasser@qu.edu.qa>
2016-06-22 20:42:35 +0300

    Add files via upload

--- skpf.m
+++ skpf.m
@@ -10,10 +10,10 @@ function  omgz   = skpf  (z , alp )
 % -----
 % Input:
 %   dv = a vector of circle centers.
-%   qq = a vector of circle radii.
+%   qv = a vector of circle radii.
 %   n  =   the number of node in the discretization of each boundary circle.
 %   alphain = a select point in the bounded domain D.
-%   dv, dq, n, alphain: must be global
+%   dv, qv, n, alphain: must be global
 %
 %   Then, we call:
 %   vj;
@@ -28,23 +28,6 @@ function  omgz   = skpf  (z , alp )
 %   and lies on the boundary.
 %
 %
-% Copyright Mohamed Nasser, 2016
-% Please cite this function as:
-% M. M. S. Nasser, SKPFbie: a fast boundary integral equation 
-% implementation of the Schottky–Klein prime function in MATLAB, 
-% Version 1.1, 2016. https://github.com/mmsnasser/SKPFbie.
-%
-% 
-% This program is free software; you can redistribute it and/or modify 
-% it under the terms of the GNU General Public License as published by 
-% the Free Software Foundation; either version 2 of the License, or 
-% (at your option) any later version.  This program is distributed in 
-% the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
-% even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-% PARTICULAR PURPOSE.  See the GNU General Public License for more 
-% details. You should have received a copy of the GNU General Public 
-% License along with this program; 
-% if not, see <http://www.gnu.org/licenses/>.
 %%
 global n
 global alphain
